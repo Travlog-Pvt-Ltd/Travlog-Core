25f45e5e527c594a3d803ca5838feecd082a2d1a
Aryan6400 <aryan6400cool@gmail.com>
2024-03-11 13:04:52 +0530

    Replaced places and activities with tags model and added tag search api

--- models/activities.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const activitySchema = new mongoose.Schema({
-  name: String,
-})
-
-const Activity = mongoose.model("Activity", activitySchema);
-
-export default Activity;
--- models/places.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const placeSchema = new mongoose.Schema({
-  name: String,
-})
-
-const Place = mongoose.model("Place", placeSchema);
-
-export default Place;
