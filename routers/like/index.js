d8499eca661c8cb533b63914f73a8abf1f65e4a5
Aryan6400 <aryan6400cool@gmail.com>
2024-03-26 13:24:38 +0530

    Added like and comment on comments API and changed comment model

--- routers/like/index.js
+++ routers/like/index.js
@@ -1,10 +1,12 @@
 import express from "express"
 import auth from "../../middlewares/auth.js"
-import { dislikeBlog, likeBlog } from "../../controllers/like/index.js"
+import { dislikeBlog, dislikeComment, likeBlog, likeComment } from "../../controllers/like/index.js"
 
 const likeRouter = express.Router()
 
 likeRouter.patch("/blog/like", auth, likeBlog)
 likeRouter.patch("/blog/dislike", auth, dislikeBlog)
+likeRouter.patch("/comment/like", auth, likeComment)
+likeRouter.patch("/comment/dislike", auth, dislikeComment)
 
 export default likeRouter
